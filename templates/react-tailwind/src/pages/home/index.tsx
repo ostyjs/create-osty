f9142ccf61ec9d05974b4d24edfb707c99301889
Sepehr Safari <safari.sepehr@gmail.com>
2024-07-20 18:23:51 +0330

    remove react-tailwind template

--- templates/react-tailwind/src/pages/home/index.tsx
+++
@@ -1,7 +0,0 @@
-export const HomePage = () => {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
-};
--- templates/react-tailwind/src/shared/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-type State = {};
-
-type Actions = {};
-
-export const useStore = create<State & Actions>(() => ({}));
