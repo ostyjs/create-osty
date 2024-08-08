43579165f372de576e542ede2fc6c52df4ce2e9b
sepehr-safari <safari.sepehr@gmail.com>
2024-08-09 03:12:39 +0330

    update login example component

--- templates/react-shadcn/src/pages/home/example-components/login.tsx
+++ templates/react-shadcn/src/pages/home/example-components/login.tsx
@@ -1,9 +1,21 @@
-import { Button } from '@/shared/components/ui/button';
+import { useActiveUser, useLogin } from 'nostr-hooks';
+import { useEffect } from 'react';
 
+import { Button } from '@/shared/components/ui/button';
 import { useLoginParam } from '@/shared/hooks';
 
 export const Login = () => {
+  const { activeUser } = useActiveUser();
   const { openLoginModal } = useLoginParam();
+  const { reLoginFromLocalStorage, logout } = useLogin();
 
-  return <Button onClick={() => openLoginModal()}>Login</Button>;
+  useEffect(() => {
+    reLoginFromLocalStorage({});
+  }, []);
+
+  if (activeUser) {
+    return <Button onClick={() => logout()}>Logout</Button>;
+  } else {
+    return <Button onClick={() => openLoginModal()}>Login</Button>;
+  }
 };
