7a2dafe3454b1f6cf737098ab432a74623cd3e7b
sepehr-safari <safari.sepehr@gmail.com>
2024-12-28 11:34:51 +0330

    refactor HomePage and Layout components for improved user experience and active user handling

--- templates/react-shadcn/src/pages/home/index.tsx
+++ templates/react-shadcn/src/pages/home/index.tsx
@@ -1,64 +1,20 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
-import { useActiveUser, useNdk } from 'nostr-hooks';
+import { useActiveUser } from 'nostr-hooks';
 
-import { ModeToggle } from '@/shared/components/mode-toggle';
-import { Button } from '@/shared/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/shared/components/ui/card';
-import { Separator } from '@/shared/components/ui/separator';
-
-import { LoginWidget } from '@/features/login-widget';
-import { LogoutWidget } from '@/features/logout-widget';
-import { ZapWidget } from '@/features/zap-widget';
+import { NotesFeedWidget } from '@/features/notes-feed-widget';
 
 export const HomePage = () => {
-  const { ndk } = useNdk();
   const { activeUser } = useActiveUser();
-
-  const zapTarget = ndk?.getUser({
-    pubkey: '3e294d2fd339bb16a5403a86e3664947dd408c4d87a0066524f8a573ae53ca8e', // Sep
-  });
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-full h-full">
-        <Card>
-          <CardHeader>
-            <CardTitle>Osty</CardTitle>
-            <CardDescription>Start building on Nostr in seconds with Osty</CardDescription>
-          </CardHeader>
-
-          <Separator />
-
-          <CardContent className="mt-6 flex gap-4">
-            {zapTarget && <ZapWidget target={zapTarget} />}
-
-            {activeUser ? <LogoutWidget /> : <LoginWidget />}
-          </CardContent>
-
-          <Separator />
-
-          <CardFooter className="mt-6 flex items-center gap-2">
-            <p className="text-xs text-gray-500">
-              Made with <span className="text-red-500">💜</span> by Sepehr
-            </p>
-
-            <Button variant="outline" size="icon" asChild className="ml-auto">
-              <a href="https://github.com/ostyjs/create-osty" target="_blank" rel="noreferrer">
-                <GitHubLogoIcon />
-              </a>
-            </Button>
-
-            <ModeToggle />
-          </CardFooter>
-        </Card>
-      </div>
+      {activeUser ? (
+        <NotesFeedWidget />
+      ) : (
+        <div className="flex flex-col h-full w-full items-center justify-center">
+          <h3>Welcome to Osty!</h3>
+          <p>Log-in to view and create notes</p>
+        </div>
+      )}
     </>
   );
 };
--- templates/react-shadcn/src/pages/index.tsx
+++ templates/react-shadcn/src/pages/index.tsx
@@ -1,9 +1,67 @@
-import { Outlet, createBrowserRouter } from 'react-router-dom';
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import { useActiveUser, useNdk } from 'nostr-hooks';
+import { Link, Outlet, createBrowserRouter } from 'react-router-dom';
+
+import { ModeToggle } from '@/shared/components/mode-toggle';
+import { Button } from '@/shared/components/ui/button';
+
+import { ActiveUserWidget } from '@/features/active-user-widget';
+import { LoginWidget } from '@/features/login-widget';
+import { ZapWidget } from '@/features/zap-widget';
 
 const Layout = () => {
+  const { ndk } = useNdk();
+  const { activeUser } = useActiveUser();
+
+  const sepehr = ndk?.getUser({
+    pubkey: '3e294d2fd339bb16a5403a86e3664947dd408c4d87a0066524f8a573ae53ca8e',
+  });
+
   return (
     <>
-      <Outlet />
+      <div className="flex flex-col justify-between h-full w-full max-w-screen-md mx-auto">
+        <div className="flex justify-between items-center border-b p-2">
+          <div>
+            <h2>Osty</h2>
+            <p>Start building on Nostr in seconds with Osty</p>
+          </div>
+
+          <div>{activeUser ? <ActiveUserWidget /> : <LoginWidget />}</div>
+        </div>
+
+        <div className="border-b h-full w-full overflow-y-auto">
+          <Outlet />
+        </div>
+
+        <div className="p-2 flex items-center justify-between">
+          <div className="text-xs flex items-center gap-1">
+            <span>Made with</span>
+            <span className="text-red-500">💜</span>
+            <span>by</span>
+            <Link to={`/profile/${sepehr?.npub}`} className="hover:underline">
+              Sepehr
+            </Link>
+          </div>
+
+          <div className="flex items-center gap-2">
+            {sepehr && (
+              <ZapWidget target={sepehr}>
+                <Button variant="secondary" size="sm">
+                  Buy me a coffee ☕️
+                </Button>
+              </ZapWidget>
+            )}
+
+            <Button variant="outline" size="icon" asChild className="ml-auto">
+              <a href="https://github.com/ostyjs/create-osty" target="_blank" rel="noreferrer">
+                <GitHubLogoIcon />
+              </a>
+            </Button>
+
+            <ModeToggle />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
