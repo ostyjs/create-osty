44b4f461c290deb3fe4295d0de7ed885bb449da3
sepehr-safari <safari.sepehr@gmail.com>
2025-01-21 22:46:25 +0330

    Refactor FollowUnfollowBtn component to use memoization for improved performance

--- templates/nostribe/src/features/user-profile-widget/components/follow-unfollow-btn/index.tsx
+++ templates/nostribe/src/features/user-profile-widget/components/follow-unfollow-btn/index.tsx
@@ -1,44 +1,49 @@
 import { NDKUser } from '@nostr-dev-kit/ndk';
 import { useFollows } from 'nostr-hooks';
+import { memo, useMemo } from 'react';
 
 import { Button } from '@/shared/components/ui/button';
 
-export const FollowUnfollowBtn = ({
-  targetUser,
-  activeUser,
-}: {
-  targetUser: NDKUser;
-  activeUser: NDKUser;
-}) => {
-  const { follows } = useFollows({ pubkey: activeUser.pubkey });
+export const FollowUnfollowBtn = memo(
+  ({ targetUser, activeUser }: { targetUser: NDKUser; activeUser: NDKUser }) => {
+    const { follows } = useFollows({ pubkey: activeUser.pubkey });
 
-  if (activeUser.pubkey === targetUser.pubkey) {
-    return null;
-  }
+    const isFollowed = useMemo(
+      () => follows?.some((u) => u.pubkey === targetUser.pubkey),
+      [follows, targetUser.pubkey],
+    );
 
-  if (follows?.some((u) => u.pubkey === targetUser.pubkey)) {
-    return (
-      <>
-        <Button
-          variant="secondary"
-          className="rounded-full"
-          onClick={() => activeUser.unfollow(targetUser)}
-        >
-          Unfollow
-        </Button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Button
-          variant="secondary"
-          className="rounded-full"
-          onClick={() => activeUser.follow(targetUser)}
-        >
-          Follow
-        </Button>
-      </>
-    );
-  }
-};
+    if (activeUser.pubkey === targetUser.pubkey) {
+      return null;
+    }
+
+    if (isFollowed) {
+      return (
+        <>
+          <Button
+            variant="secondary"
+            className="rounded-full"
+            onClick={() => activeUser.unfollow(targetUser)}
+          >
+            Unfollow
+          </Button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Button
+            variant="secondary"
+            className="rounded-full"
+            onClick={() => activeUser.follow(targetUser)}
+          >
+            Follow
+          </Button>
+        </>
+      );
+    }
+  },
+  (prev, next) =>
+    prev.targetUser.pubkey === next.targetUser.pubkey &&
+    prev.activeUser.pubkey === next.activeUser.pubkey,
+);
