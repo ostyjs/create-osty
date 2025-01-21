798914789b9a5bae22a7a19305bcfc5fe95274d6
sepehr-safari <safari.sepehr@gmail.com>
2025-01-21 16:35:25 +0330

    Add loading and welcome states to notifications page

--- templates/nostribe/src/pages/notifications/index.tsx
+++ templates/nostribe/src/pages/notifications/index.tsx
@@ -1,6 +1,24 @@
+import { useActiveUser } from 'nostr-hooks';
+
+import { Spinner } from '@/shared/components/spinner';
+
 import { NotificationsWidget } from '@/features/notifications-widget';
 
 export const NotificationsPage = () => {
+  const { activeUser } = useActiveUser();
+
+  if (activeUser === undefined) {
+    return <Spinner />;
+  }
+
+  if (activeUser === null) {
+    return (
+      <div className="flex flex-col h-full w-full items-center justify-center">
+        <h3>Welcome to Nostribe!</h3>
+      </div>
+    );
+  }
+
   return (
     <div className="h-full w-full overflow-y-auto">
       <NotificationsWidget />
